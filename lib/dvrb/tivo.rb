cb2e463ab74769022306b1c14c9fc8bd34dc5cb0
Marshall Huss <mwhuss@gmail.com>
2009-06-12 21:05:14 -0400

    Added more tivo commands and implemented remote function

--- lib/dvrb/tivo.rb
+++ lib/dvrb/tivo.rb
@@ -2,32 +2,146 @@ class DVRB::Tivo < DVRB::Base
   
   PORT = 31339
   
+  attr_accessor :tivo
+  
   def initialize(host)
     @host = host
   end
   
-  def channel_up
-    send_to_tivo "CHANNELUP"
-  end
-  
-  def channel_down
-    send_to_tivo "CHANNELDOWN"
-  end
-  
-  def pause
-    send_to_tivo "PAUSE"
-  end
-  
+  # Play
   def play
-    send_to_tivo "PLAY"
+    remote "PLAY"
   end
   
+  # Pause
+  def pause
+    remote "PAUSE"
+  end
+  
+  # Starts recording the current channel
+  def record
+    remote "RECORD", "SELECT"
+  end
+  
+  # Channel up
+  def channel_up
+    remote "CHANNELUP"
+  end
+  
+  # Channel down
+  def channel_down
+    remote "CHANNELDOWN"
+  end
+  
+  def select
+    remote "SELECT"
+  end
+  
+  # Input a number
+  def number(num)
+    remote "NUM#{num}"
+  end
+  
+  # Changes the channel
+  def channel(num)
+    num.to_s.split("").each { |n| number(n) }
+  end
+  
+  def up
+    remote "UP"
+  end
+     
+  def down
+    remote "DOWN"
+  end
+  
+  def left
+    remote "LEFT"
+  end
+  
+  def right
+    remote "RIGHT"
+  end
+
+  # TiVo specific functions
+  def tivo
+    remote "TIVO"
+  end
+  
+  def thumbs_up
+    remote "THUMBSUP"
+  end
+  
+  def thumbs_down
+    remote "THUMBSDOWN"
+  end
+  
+  def live_tv
+    remote "LIVETV"
+  end
+  
+  def now_showing
+    remote "NOWSHOWING"
+  end
+
+
+
   private
+  
+    def remote(*commands)
+      commands.each do |c|
+        send_to_tivo c
+        sleep 1
+      end
+    end
   
     def send_to_tivo(command)
       tivo = Net::Telnet.new("Host" => @host, 'Port' => PORT, "Timeout" => 5, "Prompt" => /LOCAL/)
-      tivo.cmd "IRCODE #{command}" 
+      tivo.cmd "IRCODE #{command}"
       tivo.close
     end
   
 end
+
+
+ 
+# DISPLAY 
+# DIRECTV 
+# ENTER 
+# CLEAR 
+# SLOW 
+# FORWARD 
+# REVERSE 
+# STANDBY 
+# NOWSHOWING 
+# REPLAY 
+# ADVANCE 
+# DELIMITER 
+# GUIDE
+
+
+# DONE
+# TIVO
+# NUM0 
+# NUM1 
+# NUM2 
+# NUM3 
+# NUM4 
+# NUM5 
+# NUM6 
+# NUM7 
+# NUM8 
+# NUM9
+# LIVETV 
+# THUMBSUP 
+# THUMBSDOWN 
+# CHANNELUP 
+# CHANNELDOWN
+# PLAY 
+# PAUSE
+# RECORD 
+# UP 
+# DOWN 
+# LEFT 
+# RIGHT
+# SELECT
